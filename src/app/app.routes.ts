9773913faaf142987743deac64398aa8f6cbb05b
Nabilah Jalil <22FTT1506@student.pb.edu.bn>
2024-02-28 18:13:09 +0800

    second page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'third-page', //change the path name next time to change pages (for now)
     pathMatch: 'full',
   },
   {
--- src/app/second-page/second-page.page.ts
+++ src/app/second-page/second-page.page.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonCardHeader, IonCard, IonIcon, IonButton, IonCardTitle, IonCardContent, IonImg, IonFooter, IonSearchbar, IonTabs, IonTabButton, IonLabel, IonTabBar } from '@ionic/angular/standalone';
+// import { CommonModule } from '@angular/common'; IonicModule, CommonModule, FormsModule,
+// import { FormsModule } from '@angular/forms';
+// import { IonicModule } from '@ionic/angular';
+//import { addIcons } from 'ionicons';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonCardHeader, IonCard, IonIcon, IonButton, IonCardTitle, IonCardContent, IonImg, IonFooter, IonSearchbar, IonTabs, IonTabButton, IonLabel, IonTabBar, IonGrid, IonRow, IonCol, IonList, IonCardSubtitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-second-page',
   templateUrl: './second-page.page.html',
   styleUrls: ['./second-page.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, IonTabs, IonTabBar, IonLabel, IonTabButton, IonTabs, IonSearchbar, IonFooter, IonImg, IonCardContent, IonCardTitle, IonButton, IonIcon, IonCard, IonCardHeader, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonTabs, IonTabBar, IonLabel, IonTabButton, IonTabs, IonSearchbar, IonFooter, IonImg, IonCardContent, IonCardTitle, IonButton, IonIcon, IonCard, IonCardHeader, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonList, IonCardSubtitle]
 })
-export class SecondPagePage implements OnInit {
+export class SecondPagePage{
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  // ngOnInit() { implements OnInit 
+  // }
 
 }
