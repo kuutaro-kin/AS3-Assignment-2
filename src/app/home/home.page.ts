49f55241c6e00d749a1a4ada0818c7a5b291e15a
Nabilah Jalil <22FTT1506@student.pb.edu.bn>
2024-02-28 16:30:25 +0800

    added route to secondpage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonCardHeader, IonCard, IonIcon, IonButton, IonCardTitle, IonCardContent, IonImg, IonFooter, IonSearchbar, IonTabs, IonTabButton, IonLabel, IonTabBar } from '@ionic/angular/standalone';
-
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,7 +9,13 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonCardHeader,
   imports: [IonTabs, IonTabBar, IonLabel, IonTabButton, IonTabs, IonSearchbar, IonFooter, IonImg, IonCardContent, IonCardTitle, IonButton, IonIcon, IonCard, IonCardHeader, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() { }
+  constructor(private router: Router) { 
 
+  }
+  navigateToSecondPage() {
+  this.router.navigateByUrl('/second');
+  }
   
 }
+
+
--- src/app/second-page/second-page.page.ts
+++ src/app/second-page/second-page.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonCardHeader, IonCard, IonIcon, IonButton, IonCardTitle, IonCardContent, IonImg, IonFooter, IonSearchbar, IonTabs, IonTabButton, IonLabel, IonTabBar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-second-page',
   templateUrl: './second-page.page.html',
   styleUrls: ['./second-page.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonTabs, IonTabBar, IonLabel, IonTabButton, IonTabs, IonSearchbar, IonFooter, IonImg, IonCardContent, IonCardTitle, IonButton, IonIcon, IonCard, IonCardHeader, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class SecondPagePage implements OnInit {
 
